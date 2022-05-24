1391251cbd31c8be99d762c0d45842ddd7eab980
Vijay Sam <vijay203.vk@gmail.com>
2022-05-24 21:49:25 +0530

    Started with routing and login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,30 @@
 import "./Navbar.css";
-
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Login from "./Login";
+import Compiler from "./Compiler";
 function Navbar() {
   return (
     <div>
-      <nav>
+      <BrowserRouter>
         <ul>
           <li>
-            <a id="sitename" href="#">
-              Collab Compiler
-            </a>
+            <h2 id="sitename">Collab Compiler</h2>
           </li>
           <li>
-            <a href="#">Code</a>
+            <Link to={"/compiler"}>Editor</Link>
           </li>
           <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+          {/* <li>
             <a href="#">Projects</a>
-          </li>
+          </li> */}
         </ul>
-      </nav>
+        <Routes>
+          <Route exact path="/compiler" element={<Compiler />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
